3a7aa65b26b90d93807bdc629f9778fccaf48819
KayeJin <pengjin250@gmail.com>
2018-11-05 19:40:14 +0800

    main.c

---
+++ binarytree/binarytreemain.c
@@ -0,0 +1,26 @@
+#include "binarytree.h"
+
+int main()
+{
+    tnode_p root = NULL;
+    root = BuildTree(root,1,4);
+
+    treeFrontVisit1(root);
+    printf("\n");
+    treeFrontVisit3(root);
+    printf("\n");
+    printf("\n");
+    treeMiddleVisit1(root);
+    printf("\n");
+    treeMiddleVisit2(root);
+    printf("\n");
+    printf("\n");
+    treeEndVisit1(root);
+    printf("\n");
+    treeEndVisit2(root);
+    printf("\n");
+
+    return 0;
+}
+
+
