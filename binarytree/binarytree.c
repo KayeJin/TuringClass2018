5b1cf65f41d3a3c4bb695f72cb2e461911072525
KayeJin <pengjin250@gmail.com>
2018-11-05 19:43:30 +0800

    delete some

--- binarytree/binarytree.c
+++ binarytree/binarytree.c
@@ -227,25 +227,3 @@ void destroyTree(tnode_p root)
     root = NULL;
 }
 
-int main()
-{
-    tnode_p root = NULL;
-    root = BuildTree(root,1,4);
-
-    treeFrontVisit1(root);
-    printf("\n");
-    treeFrontVisit3(root);
-    printf("\n");
-    printf("\n");
-    treeMiddleVisit1(root);
-    printf("\n");
-    treeMiddleVisit2(root);
-    printf("\n");
-    printf("\n");
-    treeEndVisit1(root);
-    printf("\n");
-    treeEndVisit2(root);
-    printf("\n");
-
-    return 0;
-}
