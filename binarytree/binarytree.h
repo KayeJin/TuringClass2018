7864fad17eb4a22151fb3a7afd9d0791a42b64ea
KayeJin <pengjin250@gmail.com>
2018-11-05 19:47:14 +0800

    well

--- binarytree/binarytree.h
+++ binarytree/binarytree.h
@@ -9,44 +9,55 @@ typedef struct tnode* tnode_p;
 
 typedef struct snode* snode_p;
 
-typedef struct tnode;
+typedef struct tnode
+{
+    tnode_p left;
+    tnode_p right;
+    unsigned char visited;
+    int data;
+}tnode;
 
 typedef struct snode
+{
+    tnode_p p_data;
+    snode_p next;
+}snode;
 
-snode_p stackBuild()
 
-snode_p stackPush(snode_p head,tnode_p tdata)
+snode_p stackBuild();
 
-tnode_p stackTop(snode_p head)
+snode_p stackPush(snode_p head,tnode_p tdata);
 
-snode_p stackPop(snode_p head)
+tnode_p stackTop(snode_p head);
 
-int stackIsEmpty(snode_p head)
+snode_p stackPop(snode_p head);
 
-void stackDestory(snode_p head)
+int stackIsEmpty(snode_p head);
+
+void stackDestory(snode_p head);
 
 unsigned char  checker = 0;
 
-void newChecker(unsigned char num)
+void newChecker(unsigned char num);
 
-int checkVisited(tnode_p node)
+int checkVisited(tnode_p node);
 
-void visit(tnode_p node)
+void visit(tnode_p node);
 
-void treeFrontVisit1(tnode_p root)
+void treeFrontVisit1(tnode_p root);
 
-void treeFrontVisit3(tnode_p root)
+void treeFrontVisit3(tnode_p root);
  
-void treeMiddleVisit1(tnode_p root)
+void treeMiddleVisit1(tnode_p root);
 
-void treeMiddleVisit2(tnode_p root)
+void treeMiddleVisit2(tnode_p root);
 
-void treeEndVisit1(tnode_p root)
+void treeEndVisit1(tnode_p root);
 
-void treeEndVisit2(tnode_p root)
+void treeEndVisit2(tnode_p root);
 
-tnode_p BuildTree(tnode_p root,int data,int floor)
+tnode_p BuildTree(tnode_p root,int data,int floor);
 
-void destroyTree(tnode_p root)
+void destroyTree(tnode_p root);
 
 #endif
